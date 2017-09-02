fb64cb1dafd5caaaa11dca9fdf9b77ad0ebcb7ef
minhaz <minhazav@gmail.com>
2017-09-03 01:20:55 +0530

    Added cmd script to initialized 24 nodes :D Super fun experiment; try closing couple of them and bring havoc

---
+++ init.cmd
@@ -0,0 +1,32 @@
+start node process\index.js 8080
+timeout /t 1
+start node process\index.js 8081 8080
+timeout /t 1
+start node process\index.js 8082 8080
+start node process\index.js 8083 8081
+timeout /t 1
+start node process\index.js 8084 8080
+start node process\index.js 8085 8081
+start node process\index.js 8086 8081
+start node process\index.js 8087 8081
+start node process\index.js 8088 8081
+
+timeout /t 1
+start node process\index.js 8089 8080
+start node process\index.js 8090 8082
+start node process\index.js 8091 8082
+start node process\index.js 8092 8081
+start node process\index.js 8093 8081
+start node process\index.js 8094 8080
+start node process\index.js 8095 8082
+start node process\index.js 8096 8082
+start node process\index.js 8097 8087
+start node process\index.js 8098 8087
+
+timeout /t 1
+start node process\index.js 8099 8081
+start node process\index.js 8900 8080
+start node process\index.js 8901 8082
+start node process\index.js 8902 8082
+start node process\index.js 8903 8087
+start node process\index.js 8904 8087
